2ed22696899e02748143314163f3c4c75ad38bf8
Jared <jared5ocotlan@gmail.com>
2024-12-20 03:28:10 -0800

    front end accesing api

--- react-front/src/Components/Itembox.js
+++ react-front/src/Components/Itembox.js
@@ -3,14 +3,25 @@ import Item from "./item";
 
 export default Itembox;
 
+
+
 function Itembox() {
+    const [products, setProducts] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("http://localhost:8080/all")
+            .then((response) => response.json())
+            .then((data) => setProducts(data))
+    })
+
     return (
         <div className="layout-items">
-            <h1>Itembox</h1>
-            <Item />
-            <Item />
-            <Item />
-            <Item />
+                {products.map((product) => (
+                    <div key={product.id} className="item">
+                        <h3>{product.productName}</h3>
+                        <p>{product.price}</p>
+                    </div>
+                ))} 
         </div>
     );  
 };
--- react-front/src/Components/item.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default Item;
-
-function Item() {
-    return (
-        <p>item</p>
-    )
-};
