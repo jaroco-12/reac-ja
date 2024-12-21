5d02e350364357290ba6f2f52955f29e69601753
Jared <jared5ocotlan@gmail.com>
2024-12-20 17:22:25 -0800

    added detailed section on click of item

--- react-front/src/Components/Itembox.js
+++ react-front/src/Components/Itembox.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Item from "./item";
 
 export default Itembox;
 
 
 
 function Itembox() {
-    const [products, setProducts] = React.useState([])
+    const [products, setProducts] = React.useState([]);
+    const [selectedProductId, setSelectedProductID] = React.useState(null);
 
     React.useEffect(() => {
         fetch("http://localhost:8080/all")
@@ -14,10 +14,27 @@ function Itembox() {
             .then((data) => setProducts(data))
     })
 
+    const productClick = (productID) => {
+        setSelectedProductID(productID)
+    }
+
+    const backClick = () => {
+        setSelectedProductID(null)
+    }
+
+
     return (
         <div className="layout-items">
-                {products.map((product) => (
-                    <div key={product.id} className="item">
+                {products.map((product) => 
+                    selectedProductId === product.id ? (
+                        <div key={product.id} className="grid-item detail-view">
+                            <h2>{product.name}</h2>
+                            <p>Price: ${product.price}</p>
+                            <p>Description: {product.description || "No description available."}</p>
+                            <button onClick={backClick}>Back to Product List</button>
+                        </div>
+                    ) : (
+                    <div key={product.id} className="items" onClick={() => productClick(product.id)}>
                         <h3>{product.productName}</h3>
                         <p>{product.price}</p>
                     </div>
